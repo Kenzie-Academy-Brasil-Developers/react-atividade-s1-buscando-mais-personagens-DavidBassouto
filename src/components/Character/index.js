bf7bc273ca8c78763829e7da8628ea1e92f89ef2
DavidBassouto <davidbassouto@hotmail.com>
2022-06-01 11:57:24 -0300

    update: botoes mudam a paginação obtida na api, se nao ouver pag, botao é desabilitado

--- src/components/Character/index.js
+++ src/components/Character/index.js
@@ -1,15 +1,25 @@
 import CharCard from "../CharCard";
 import "./style.css";
 
-function Character({ characterList }) {
+function Character({ characterList, setPage, page }) {
   return (
     <>
       <h1>Personagens Rick and Morty</h1>
 
       <div className="container-charCard">
         <div className="container-buttons">
-          <button> Anterior </button>
-          <button> Próxima </button>
+          {page === 1 ? (
+            <button className="disabled" > Anterior </button>
+          ) : (
+            <button onClick={() => setPage(-1)}> Anterior </button>
+          )}
+          
+          {page === null ? (
+            <button className="disabled"> Próxima </button>
+          ) : (
+            <button onClick={() => setPage(1)}> Próxima </button>
+          )}
+          
         </div>
         {characterList.map((char) => (
           <CharCard char={char} key={char.id} />
--- src/pages/home.js
+++ src/pages/home.js
@@ -3,15 +3,20 @@ import Character from "../components/Character";
 
 function Home() {
   const [characterList, setCharacterList] = useState([]);
+  const [page, setPage] = useState(1);
+  function handlePage(number){
+    setPage(page+number)
+  }
 
   useEffect(() => {
-    fetch("https://rickandmortyapi.com/api/character/")
+    fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
       .then((res) => res.json())
       .then((res) => setCharacterList(res.results));
-  }, []);
+  },[page]);
+  console.log(page)
   return (
     <>
-      <Character characterList={characterList} />
+      <Character characterList={characterList} setPage={handlePage} page={page}/>
     </>
   );
 }
