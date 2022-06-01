5e3889a9d6fba3183e77ce169f52cf4bfd49eb49
DavidBassouto <davidbassouto@hotmail.com>
2022-06-01 12:38:52 -0300

    update:mudança nome doc e icon

--- src/components/Character/index.js
+++ src/components/Character/index.js
@@ -1,42 +1,40 @@
 import CharCard from "../CharCard";
 import "./style.css";
 
-function Character({ characterList, setPage, page }) {
+function Character({ characterList, setPage, thisPage }) {
   return (
     <>
       <h1>Personagens Rick and Morty</h1>
 
       <div className="container-charCard">
         <div className="container-buttons">
-          {page === 1 ? (
-            <button className="disabled" > Anterior </button>
+          {thisPage.prev === null ? (
+            <button className="disabled"> Anterior </button>
           ) : (
             <button onClick={() => setPage(-1)}> Anterior </button>
           )}
-          
-          {page === 42 ? (
+
+          {thisPage.next === null ? (
             <button className="disabled"> Próxima </button>
           ) : (
             <button onClick={() => setPage(1)}> Próxima </button>
           )}
-          
         </div>
         {characterList.map((char) => (
           <CharCard char={char} key={char.id} />
         ))}
         <div className="container-buttons">
-          {page === 1 ? (
-            <button className="disabled" > Anterior </button>
+          {thisPage.prev === null ? (
+            <button className="disabled"> Anterior </button>
           ) : (
             <button onClick={() => setPage(-1)}> Anterior </button>
           )}
-          
-          {page === 42 ? (
+
+          {thisPage.next === null ? (
             <button className="disabled"> Próxima </button>
           ) : (
             <button onClick={() => setPage(1)}> Próxima </button>
           )}
-          
         </div>
       </div>
     </>
--- src/pages/home.js
+++ src/pages/home.js
@@ -4,19 +4,30 @@ import Character from "../components/Character";
 function Home() {
   const [characterList, setCharacterList] = useState([]);
   const [page, setPage] = useState(1);
-  function handlePage(number){
-    setPage(page+number)
+  const [thisPage, setThisPage] = useState(false);
+  function handlePage(number) {
+    setPage(page + number);
   }
 
   useEffect(() => {
     fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
       .then((res) => res.json())
       .then((res) => setCharacterList(res.results));
-  },[page]);
-  console.log(page)
+  }, [page]);
+
+  useEffect(() => {
+    fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
+      .then((res) => res.json())
+      .then((res) => setThisPage(res.info));
+  }, [page]);
+
   return (
     <>
-      <Character characterList={characterList} setPage={handlePage} page={page}/>
+      <Character
+        characterList={characterList}
+        setPage={handlePage}
+        thisPage={thisPage}
+      />
     </>
   );
 }
