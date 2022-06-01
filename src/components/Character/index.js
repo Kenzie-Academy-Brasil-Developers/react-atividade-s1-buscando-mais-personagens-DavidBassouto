1c44096733498638dad7757e30067e97ed8e7372
DavidBassouto <davidbassouto@hotmail.com>
2022-06-01 09:37:42 -0300

    update: adicionei botao estatico e estilizacao

--- src/components/Character/index.js
+++ src/components/Character/index.js
@@ -5,7 +5,12 @@ function Character({ characterList }) {
   return (
     <>
       <h1>Personagens Rick and Morty</h1>
+
       <div className="container-charCard">
+        <div className="container-buttons">
+          <button> Anterior </button>
+          <button> Próxima </button>
+        </div>
         {characterList.map((char) => (
           <CharCard char={char} key={char.id} />
         ))}
